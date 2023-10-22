0abc9f7474b5dce1efa1cd51c88fba70574c8f15
Pacifique Clement Ndayisenga <32579116+Paccy10@users.noreply.github.com>
2023-10-22 14:53:21 +0200

    [feature]: Complete transaction

--- src/wallet/constants/messages/error.ts
+++ src/wallet/constants/messages/error.ts
@@ -1,3 +1,9 @@
 export const WALLET_EXISTS = 'User already has a wallet';
 export const WALLET_NOT_FOUND = 'Wallet not found';
 export const INSUFFICIENT_BALANCE = 'Wallet has insufficient balance';
+export const INVALID_PIN = 'Invalid pin. The pin should be 5 digits';
+export const INCORRECT_PIN = 'Incorrect pin';
+export const TRANSACTION_NOT_FOUND = 'Transaction not found';
+export const FORBIDDEN_TRANSACTION =
+  "Transaction doesn't belong to the current user";
+export const TRANSACTION_ALREADY_COMPLETED = 'Transaction already completed';
